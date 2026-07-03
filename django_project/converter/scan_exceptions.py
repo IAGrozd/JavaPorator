940789fe8f9169d0f41b63b2115a094377161c14
Ivan Grozdanov <iagrozd@gmail.com>
2026-07-03 16:22:33 +0300

    Adding custom exceptions

---
+++ django_project/converter/scan_exceptions.py
@@ -0,0 +1,10 @@
+import Exception
+
+class ConversionError(Exception):
+    def __init__(self, msg="Възникна грешка при конвертирането на кода."):
+        super().__init__(msg)
+
+
+class JavaSyntaxError(ConversionError):
+    def __init__(self, msg: str):
+        super().__init__(msg)
